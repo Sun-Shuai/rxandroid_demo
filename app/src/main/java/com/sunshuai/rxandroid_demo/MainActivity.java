52b558732cefb61847a4c3360afbb4d4c9ff3560
sunshuai <438819781>
2018-01-31 14:48:38 +0800

    simple0

--- app/src/main/java/com/sunshuai/rxandroid_demo/MainActivity.java
+++ app/src/main/java/com/sunshuai/rxandroid_demo/MainActivity.java
@@ -1,13 +1,65 @@
 package com.sunshuai.rxandroid_demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                emitter.onNext(4);
+                emitter.onNext(5);
+                emitter.onNext(6);
+                emitter.onComplete();
+            }
+        }).subscribe(new Observer<Integer>() {
+
+            private int i;
+            private Disposable disposable;
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                disposable = d;
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.e(TAG, String.valueOf(i));
+                i++;
+
+                if (i == 3){
+                    disposable.dispose();
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+            }
+
+            @Override
+            public void onComplete() {
+            }
+        });
+
     }
 }
